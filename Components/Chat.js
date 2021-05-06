28798f9ac1bfd2a6138f5f98d586cd01e0bb7a85
aldoecheverria <aldoecheverria@yahoo.com.mx>
2021-05-06 16:18:01 -0500

    first commit of may 6

--- Components/Chat.js
+++ Components/Chat.js
@@ -1,8 +1,12 @@
 import styled from 'styled-components';
 import { Avatar } from '@material-ui/core';
+import getRecipientEmail from '../utils/getRecipientEmail';
 
 
 function Chat({id, users}) {
+
+    const recipientEmail = getRecipientEmail(users, user);
+
     return (
         <Container>
             <UserAvatar />
